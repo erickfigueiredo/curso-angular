fee63b1c333e7fecc27e88fe0cf41a28077cb50b
erickfigueiredo <ericklima816@gmail.com>
2023-02-17 17:51:06 -0300

    Add route concepts

---
+++ routing-start/src/app/servers/servers.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { ServersService } from './servers.service';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css'],
+})
+export class ServersComponent implements OnInit {
+  public servers: { id: number; name: string; status: string }[] = [];
+
+  constructor(
+    private serversService: ServersService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit() {
+    this.servers = this.serversService.getServers();
+  }
+
+  onReload() {
+    // Navigate unlike routerLink doesn't know where we are, so
+    // Below: It'll break the app because we don't have /servers/servers
+    this.router.navigate(['servers'], { relativeTo: this.route });
+  }
+}
