a8ee85025b50005fc155ca635c90929205a6571d
Erick Figueiredo <ericklima816@gmail.com>
2023-02-24 23:04:10 -0300

    Adiciona guards de acesso para rotas

--- routing-start/src/app/app.module.ts
+++ routing-start/src/app/app.module.ts
@@ -1,8 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { Routes, RouterModule } from '@angular/router';
-
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { UsersComponent } from './users/users.component';
@@ -11,29 +9,13 @@ import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServersService } from './servers/servers.service';
 import { ServerComponent } from './servers/server/server.component';
-
-const appRoutes: Routes = [
-  {
-    path: '',
-    component: HomeComponent,
-  },
-  {
-    path: 'users',
-    component: UsersComponent,
-  },
-  {
-    path: 'users/:id/:name',
-    component: UsersComponent,
-  },
-  {
-    path: 'servers',
-    component: ServersComponent,
-  },
-  {
-    path: 'servers/:id/edit',
-    component: EditServerComponent,
-  },
-];
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AuthService } from './auth.service';
+import { AuthGuard } from './auth-guard.service';
+import { CanDeactivateGuard } from './servers/edit-server/can-deactivate-guard.service';
+import { ErrorPageComponent } from './error-page/error-page.component';
+import { ServerResolver } from './servers/server/server-resolver.service';
 
 @NgModule({
   declarations: [
@@ -44,9 +26,11 @@ const appRoutes: Routes = [
     ServerComponent,
     UserComponent,
     EditServerComponent,
+    PageNotFoundComponent,
+    ErrorPageComponent,
   ],
-  imports: [BrowserModule, FormsModule, RouterModule.forRoot(appRoutes)],
-  providers: [ServersService],
+  imports: [BrowserModule, FormsModule, AppRoutingModule],
+  providers: [ServersService, AuthService, AuthGuard, CanDeactivateGuard, ServerResolver],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
--- routing-start/src/app/servers/server/server.component.ts
+++ routing-start/src/app/servers/server/server.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ServersService } from '../servers.service';
+import { ActivatedRoute, Params, Router, Data } from '@angular/router';
 
 @Component({
   selector: 'app-server',
@@ -8,12 +9,30 @@ import { ServersService } from '../servers.service';
   styleUrls: ['./server.component.css']
 })
 export class ServerComponent implements OnInit {
-  server: {id: number, name: string, status: string};
+  server: { id: number, name: string, status: string };
 
-  constructor(private serversService: ServersService) { }
+  constructor(private serversService: ServersService,
+    private route: ActivatedRoute,
+    private router: Router) { }
 
   ngOnInit() {
-    this.server = this.serversService.getServer(1);
+    /*     const id = parseInt(this.route.snapshot.params['id']);
+        this.server = this.serversService.getServer(id);
+
+        this.route.params.subscribe((params: Params) => {
+          this.server = this.serversService.getServer(parseInt(params['id']));
+        }); */
+
+    // Acessando o resolver
+    this.route.data.subscribe((data: Data) => {
+      this.server = data.server;
+    });
   }
 
+  onEdit() {
+    this.router.navigate(['edit'], {
+      relativeTo: this.route,
+      queryParamsHandling: 'preserve' // Importante, espera palavras chaves
+    });
+  }
 }
