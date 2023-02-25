d741aa64a7f2d12fa268de0ff7f918735ce4d6d9
erickfigueiredo <ericklima816@gmail.com>
2023-02-25 16:41:00 -0300

    Completa a implementação de rotas no list app

--- list-app/src/app/recipes/recipes-detail/recipes-detail.component.ts
+++ list-app/src/app/recipes/recipes-detail/recipes-detail.component.ts
@@ -1,19 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../recipes.model';
 import { RecipeService } from '../recipe.service';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 @Component({
   selector: 'app-recipes-detail',
   templateUrl: './recipes-detail.component.html',
   styleUrls: ['./recipes-detail.component.css']
 })
-export class RecipesDetailComponent implements OnInit{
+export class RecipesDetailComponent implements OnInit {
   recipe: Recipe;
   id: number;
-  constructor(private recipeService: RecipeService, private route: ActivatedRoute) { }
+  constructor(private recipeService: RecipeService, private route: ActivatedRoute, private router: Router) { }
   ngOnInit(): void {
-    this.route.params.subscribe((params:Params) =>{
+    this.route.params.subscribe((params: Params) => {
       this.id = parseInt(params['id']);
       this.recipe = this.recipeService.getRecipeById(this.id);
     });
@@ -21,5 +21,10 @@ export class RecipesDetailComponent implements OnInit{
 
   onAddToShoppingList() {
     this.recipeService.addIngredientToShoppingList(this.recipe.ingredients)
+  }
+
+  onEditRecipe() {
+    //this.router.navigate(['edit'], { relativeTo: this.route });
+    this.router.navigate(['../', this.id, 'edit'], { relativeTo: this.route });
   }
 }
