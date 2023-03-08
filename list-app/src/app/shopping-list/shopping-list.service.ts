9dfd7111f558b436357ef5fcc5b0fdffaecbbd6d
erickfigueiredo <ericklima816@gmail.com>
2023-03-08 10:48:28 -0300

    Adiciona observaveis ao App principal

--- list-app/src/app/shopping-list/shopping-list.service.ts
+++ list-app/src/app/shopping-list/shopping-list.service.ts
@@ -1,26 +1,26 @@
-import {EventEmitter} from "@angular/core";
-import { Ingredient } from "../shared/ingredient.model";
+import { Subject } from 'rxjs';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class ShoppingListService {
-  ingredientsChanged = new EventEmitter<Ingredient[]>();
-  
+  ingredientsChanged = new Subject<Ingredient[]>();
+
   private ingredients: Ingredient[] = [
     new Ingredient('Apples', 5),
     new Ingredient('tomatoes', 5),
     new Ingredient('tomatoes', 5),
   ];
 
-  getIngredients(): Ingredient[]{
+  getIngredients(): Ingredient[] {
     return this.ingredients.slice();
   }
 
   addIngredient(ingredient: Ingredient): void {
-    this.ingredients.push(ingredient)
-    this.ingredientsChanged.emit(this.ingredients.slice());
+    this.ingredients.push(ingredient);
+    this.ingredientsChanged.next(this.ingredients.slice());
   }
 
   addIngredients(ingredients: Ingredient[]) {
-    this.ingredients.push(...ingredients)
-    this.ingredientsChanged.emit(this.ingredients.slice());
+    this.ingredients.push(...ingredients);
+    this.ingredientsChanged.next(this.ingredients.slice());
   }
 }
